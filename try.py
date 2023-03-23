f70e2468da533f0067fc974f31ad5fef4dc52462
Bwiza-dalia <50427468+Bwiza-dalia@users.noreply.github.com>
2023-03-23 11:21:37 +0200

    Add files via upload
    first commit

---
+++ try.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.figure_factory as ff
+
